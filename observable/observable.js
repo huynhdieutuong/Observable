66f5b023ab8017489c362d1e9d8e3343bd50a4f1
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-08-08 16:01:28 +0700

    Apply Symbol to hidden _subscribe property && IIFE to prevent users access Observable.timeout(1000)[SymbolSubscribe]

--- observable/observable.js
+++ observable/observable.js
@@ -1,55 +1,61 @@
-class Observable {
-  constructor(_subscribe) {
-    this._subscribe = _subscribe
-  }
+window.Observable = (function () {
+  const SymbolSubscribe = Symbol('Subscribe')
 
-  subscribe(next, error, complete) {
-    let observer
-    if (typeof next === 'function') {
-      observer = {
-        next: next,
-        error: error || function () {},
-        complete: complete || function () {},
-      }
-    } else {
-      observer = next
-      if (!observer.error) observer.error = function () {}
-      if (!observer.complete) observer.complete = function () {}
+  class Observable {
+    constructor(_subscribe) {
+      this[SymbolSubscribe] = _subscribe
     }
 
-    return this._subscribe(observer)
-  }
-
-  static timeout(miliseconds) {
-    function _subscribe(observer) {
-      const timeoutId = setTimeout(() => {
-        observer.next()
-        observer.complete()
-      }, miliseconds)
-
-      return {
-        unsubsribe() {
-          clearTimeout(timeoutId)
-        },
+    subscribe(next, error, complete) {
+      let observer
+      if (typeof next === 'function') {
+        observer = {
+          next: next,
+          error: error || function () {},
+          complete: complete || function () {},
+        }
+      } else {
+        observer = next
+        if (!observer.error) observer.error = function () {}
+        if (!observer.complete) observer.complete = function () {}
       }
+
+      return this[SymbolSubscribe](observer)
     }
 
-    return new Observable(_subscribe)
-  }
+    static timeout(miliseconds) {
+      function _subscribe(observer) {
+        const timeoutId = setTimeout(() => {
+          observer.next()
+          observer.complete()
+        }, miliseconds)
 
-  static interval(miliseconds) {
-    function _subscribe(observer) {
-      const intervalId = setInterval(() => {
-        observer.next()
-      }, miliseconds)
-
-      return {
-        unsubsribe() {
-          clearInterval(intervalId)
-        },
+        return {
+          unsubsribe() {
+            clearTimeout(timeoutId)
+          },
+        }
       }
+
+      return new Observable(_subscribe)
     }
 
-    return new Observable(_subscribe)
+    static interval(miliseconds) {
+      function _subscribe(observer) {
+        const intervalId = setInterval(() => {
+          observer.next()
+        }, miliseconds)
+
+        return {
+          unsubsribe() {
+            clearInterval(intervalId)
+          },
+        }
+      }
+
+      return new Observable(_subscribe)
+    }
   }
-}
+
+  return Observable
+})()
