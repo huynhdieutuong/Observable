46515538ce936439297863bb5b20f0645a053865
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-08-08 15:26:07 +0700

    Separate the common code

--- observable/observable.js
+++ observable/observable.js
@@ -1,23 +1,27 @@
 class Observable {
-  constructor(subscribe) {
-    this.subscribe = subscribe
+  constructor(_subscribe) {
+    this._subscribe = _subscribe
+  }
+
+  subscribe(next, error, complete) {
+    let observer
+    if (typeof next === 'function') {
+      observer = {
+        next: next,
+        error: error || function () {},
+        complete: complete || function () {},
+      }
+    } else {
+      observer = next
+      if (!observer.error) observer.error = function () {}
+      if (!observer.complete) observer.complete = function () {}
+    }
+
+    return this._subscribe(observer)
   }
 
   static timeout(miliseconds) {
-    function subscribe(next, error, complete) {
-      let observer
-      if (typeof next === 'function') {
-        observer = {
-          next: next,
-          error: error || function () {},
-          complete: complete || function () {},
-        }
-      } else {
-        observer = next
-        if (!observer.error) observer.error = function () {}
-        if (!observer.complete) observer.complete = function () {}
-      }
-
+    function _subscribe(observer) {
       const timeoutId = setTimeout(() => {
         observer.next()
         observer.complete()
@@ -30,24 +34,11 @@ class Observable {
       }
     }
 
-    return new Observable(subscribe)
+    return new Observable(_subscribe)
   }
 
   static interval(miliseconds) {
-    function subscribe(next, error, complete) {
-      let observer
-      if (typeof next === 'function') {
-        observer = {
-          next: next,
-          error: error || function () {},
-          complete: complete || function () {},
-        }
-      } else {
-        observer = next
-        if (!observer.error) observer.error = function () {}
-        if (!observer.complete) observer.complete = function () {}
-      }
-
+    function _subscribe(observer) {
       const intervalId = setInterval(() => {
         observer.next()
       }, miliseconds)
@@ -59,6 +50,6 @@ class Observable {
       }
     }
 
-    return new Observable(subscribe)
+    return new Observable(_subscribe)
   }
 }
